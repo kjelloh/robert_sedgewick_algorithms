4b7a63781710fa53170e7d5458cfd8e86ea097d9
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-01-22 21:55:07 +0100

    Added empty chapter_0 (template) and chapter_2

---
+++ chapter_2/ch2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <functional>
+#include <memory>
+
+void chapter_2() {
+}
+
+using Skill = std::pair<std::string,std::function<void()>>;
+
+int main(int argc,char* argv[]) {
+    std::map<int,Skill> skills{
+         {0,{"chapter 20 examples",chapter_2}}
+    };
+    int exersice{0};
+    if (argc==2) {
+        exersice = std::stoi(argv[1]);
+    }
+    if (skills.contains(exersice)) skills.at(exersice).second();   
+    else {
+        std::cout << "\nUnknown argument: " << exersice;
+        std::cout << "\nAvailable options:";
+        for (auto entry : skills) {
+            std::cout << "\n\targument: " << entry.first << " to execute: " << entry.second.first;
+        }
+    } 
+    std::cout << "\n";
+    return 0;
+}
